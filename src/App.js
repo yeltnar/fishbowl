1d7863a32af50f4b0d58906c160846c41c94c701
Drew Brantley <brantley@us.ibm.com>
2019-11-14 22:38:37 -0600

    setting up the fist page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const uuid_v4 = require('uuid/v4');
+
+const too_many_ideas = "You already sent all your ideas";
+
 function App() {
+
+  let [ideas_left_count, setIdeasLeftCount] = useState(3);
+  let [idea_value, setIdeaValue] = useState("");
+  let user_idea = localStorage.getItem("fishbowl_user_id") || getNewUserId();
+  let [idea_arr, setIdeaArr] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Ideas left {ideas_left_count}</div>
+      <input type="text" value={idea_value} default="bad" onChange={ideaValueChange}></input>
+      <br/>
+      <button onClick={()=>{ addIdea(user_idea, idea_value) }}>Submit</button>
+      <br/>
+      <div>{idea_arr.join(",")}</div>
     </div>
   );
+
+  function ideaValueChange(asfd){
+    setIdeaValue(asfd.target.value);
+  }
+
+  function addIdea( user_id, idea ){
+    const new_idea_count = ideas_left_count-1;
+    if( new_idea_count<0 ){
+      alert(too_many_ideas);
+    }else{
+      setIdeasLeftCount( new_idea_count );
+      idea_arr.push(idea);
+      console.log( idea_arr );
+      setIdeaArr( idea_arr );
+      setIdeaValue("");
+    }
+  }
+  
+  function getNewUserId(){
+    return uuid_v4();
+  }
 }
 
 export default App;
